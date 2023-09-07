1956927d0d5030ea6fddbc181c91f1013e433d01
Pushpak Raj <67819822+pushpak028@users.noreply.github.com>
2023-09-07 11:42:34 +0530

    Update load_json.py

--- load_json.py
+++ load_json.py
@@ -6,5 +6,11 @@ def load1(file_path):
     with open(file_path,'r') as file:
         data : dict =  json.load(file) 
     return data
-
+def blockwords(message):
+    block_words = ["fuck","f*ck","http://","https://"]
+    for text in block_words:
+        if message.lower() == text.lower():
+            return 1
+        else:
+            return 0
 
